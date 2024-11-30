d24adedd3fa020a38f8f7a3b20711b70748cbecf
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-11-30 15:36:06 +0330

    Basic definition: [navigation bar], [@State], [Picker], [TextField], [ForEach], [Section]

--- UI1-WeSplit/ContentView.swift
+++ UI1-WeSplit/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let country = ["IRAN", "USA", "UK", "JAPAN"]
+    @State private var selectCountry = "IRAN"
+    
+    @State var tapCount = 0
+    @State var name = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form{
+                Text("Sothesom")
+                Text("iran")
+                
+                Section{
+                    Text("tehran")
+                }
+                
+                Section{
+                    Button("add \(tapCount)"){
+                        tapCount += 1
+                    }
+                }
+                
+                Section{
+                    Picker("select your contry", selection: $selectCountry){
+                        ForEach(country, id: \.self){
+                            Text($0)
+                        }
+                    }
+                }
+                
+                TextField("Enter your name", text: $name)
+                Text("Your name is \(name)")
+                
+                Section{
+                    ForEach(0..<10) { number in
+                        Text("ADAD \(number)")
+                    }
+                }
+                
+            }
+            .navigationTitle("Sothesom")
+//            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
