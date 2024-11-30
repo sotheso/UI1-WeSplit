0070d9c86c7fd53f8379ea3a6faa4002fdeb1ccd
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-11-30 15:32:41 +0330

    Initial Commit

---
+++ UI1-WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UI1-WeSplit
+//
+//  Created by Sothesom on 10/09/1403.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UI1-WeSplit/UI1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI1_WeSplitApp.swift
+//  UI1-WeSplit
+//
+//  Created by Sothesom on 10/09/1403.
+//
+
+import SwiftUI
+
+@main
+struct UI1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
